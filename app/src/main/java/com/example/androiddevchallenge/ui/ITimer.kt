4ca65020fbee710e83a4b1ae5d4c689f3c361e8e
Paulo Pereira <paulo_aa_pereira@outlook.pt>
2021-03-07 00:10:02 +0000

    # Stopless on ITimer

--- app/src/main/java/com/example/androiddevchallenge/ui/ITimer.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/ITimer.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui
 
 import com.example.androiddevchallenge.utils.getMillis
@@ -24,7 +39,7 @@ interface ITimer {
 
     var progress: Float
     var initialMillis: Long
-    
+
     fun isFinished() = isPlaying && hours == 0L && minutes == 0L && seconds == 0L
     fun isReadyToPlay() = hours > 0 || minutes > 0 || seconds > 0
     fun restart() {
